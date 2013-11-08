81216f41cf6cc15243e967ab10632742f7ea889d
Guy Rutenberg <guyrutenberg@gmail.com>
2013-11-08 10:47:58 +0200

    Handle SIGINT correctly.
    The multiprocessing module couldn't handle Ctrl-C correctly, so I wrote
    a simple process pool to replace it.

--- tree2ogg.py
+++ tree2ogg.py
@@ -3,11 +3,12 @@ import sys
 import fnmatch
 import subprocess
 import multiprocessing
+import signal
+
 
 
 def main(src_path, dst_path):
-
-    pool = multiprocessing.Pool(processes=6)
+    pool = SubprocessPool(6)
 
     for root, dirs, files in os.walk(src_path):
         flac_files = fnmatch.filter(files, "*.flac")
@@ -27,18 +28,45 @@ def main(src_path, dst_path):
         for name in flac_files:
             name_wo_ext = os.path.splitext(name)[0]
             target_name = name_wo_ext + '.ogg'
-            print target_name
         
             target_file = os.path.join(dst_path, rel_dir, target_name)
             src_file = os.path.join(root, name)
-            pool.apply_async(ogg_enc, [src_file, target_file])
+            ogg_enc(pool, src_file, target_file)
 
-    pool.close()
-    pool.join()
+        pool.wait()
 
-def ogg_enc(src_file, dst_file):
+
+def ogg_enc(pool, src_file, dst_file):
     args = ['oggenc', '--quality', '5', '--output', dst_file, src_file]
-    subprocess.call(args)
+
+    pool.popen(args)
+
+
+class SubprocessPool:
+    def __init__(self, max_jobs):
+        self.max_jobs = max_jobs
+        self.jobs = set()
+        signal.signal(signal.SIGCHLD, self._sigchld)
+
+    def _sigchld(self, signum, frame):
+        done_jobs = set()
+        for p in self.jobs:
+            if p.poll() != None:
+                done_jobs.add(p)
+
+        self.jobs.difference_update(done_jobs)
+
+    def popen(self, *args, **kwrds):
+        while len(self.jobs) >= self.max_jobs:
+            signal.pause()
+        self.jobs.add(subprocess.Popen(*args, **kwrds))
+
+    def wait(self):
+        """Wait for all the jobs to terminate"""
+        while len(self.jobs) > 0:
+            signal.pause()
+
+
 
 if __name__ == '__main__':
     main(sys.argv[1], sys.argv[2])
